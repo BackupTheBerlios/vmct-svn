5e03c9b49141703cd8f526d42d34101eb7dcb002
ababoshin <ababoshin@dd2e4168-577f-0410-a94b-e91ec466ddae>
2010-03-05 11:56:05 +0000

    Database structure changed.

--- db/db.sql
+++ db/db.sql
@@ -1,39 +1,49 @@
 CREATE TABLE IF NOT EXISTS `entry` (
-  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-  `flags` varchar(255) NOT NULL,
-  `epoch` varchar(255) NOT NULL,
-  `ver` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `flags` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `epoch` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `ver` varchar(255) COLLATE latin1_general_cs NOT NULL,
   PRIMARY KEY (`name`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 CREATE TABLE IF NOT EXISTS `file` (
-  `package_id` varchar(255) NOT NULL,
-  `name` text NOT NULL
-);
+  `package` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `name` text COLLATE latin1_general_cs NOT NULL,
+  KEY `fk1` (`package`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 CREATE TABLE IF NOT EXISTS `os` (
-  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `name` varchar(255) COLLATE latin1_general_cs NOT NULL,
   PRIMARY KEY (`name`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 CREATE TABLE IF NOT EXISTS `package` (
-  `name` varchar(255) NOT NULL,
-  `arch` varchar(255) NOT NULL,
-  `checksum` varchar(255) NOT NULL,
-  `description` text NOT NULL,
-  `version` varchar(255) NOT NULL,
-  `revision` varchar(255) NOT NULL,
-  `os_id` varchar(255) NOT NULL,
-  PRIMARY KEY (`checksum`)
-);
+  `name` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `arch` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `checksum` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `description` text COLLATE latin1_general_cs NOT NULL,
+  `version` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `revision` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `os` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  PRIMARY KEY (`checksum`),
+  KEY `fk1` (`os`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-CREATE TABLE IF NOT EXISTS `provides` (
-  `package_id` varchar(255) NOT NULL,
-  `entry_id` varchar(255) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `relation` (
+  `type` enum('provides','requires') NOT NULL,
+  `package` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  `entry` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
+  KEY `type` (`type`),
+  KEY `relation_ibfk_2` (`package`),
+  KEY `relation_ibfk_1` (`entry`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
-CREATE TABLE IF NOT EXISTS `requires` (
-  `package_id` varchar(255) NOT NULL,
-  `entry_id` varchar(255) NOT NULL
-);
+ALTER TABLE `file`
+  ADD CONSTRAINT `file_ibfk_1` FOREIGN KEY (`package`) REFERENCES `package` (`checksum`);
 
+ALTER TABLE `package`
+  ADD CONSTRAINT `package_ibfk_1` FOREIGN KEY (`os`) REFERENCES `os` (`name`);
+
+ALTER TABLE `relation`
+  ADD CONSTRAINT `relation_ibfk_2` FOREIGN KEY (`package`) REFERENCES `package` (`checksum`),
+  ADD CONSTRAINT `relation_ibfk_1` FOREIGN KEY (`entry`) REFERENCES `entry` (`name`);
